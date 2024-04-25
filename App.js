72aa2a1d7bd6188d9418e842a74bdd6f1e4a1b5e
rkdesigner66 <167181194+rkdesigner66@users.noreply.github.com>
2024-04-25 06:52:34 +0530

    test

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import InputField from './Components/InputField';
+import WebRoutes from './routes/webRoutes';
+
+function App() {
+  return (
+    <div>
+      <WebRoutes />
+    </div>
+  );
+}
+
+export default App;
